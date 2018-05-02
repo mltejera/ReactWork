1dd23b6e899256f00176f415f138a22a235309e4
Mason Tejera <mltejera@gmail.com>
2018-05-02 15:18:11 -0700

    Using BooksApi
    but running into an error on changing list

--- MyReads/src/App.js
+++ MyReads/src/App.js
@@ -2,53 +2,11 @@ import React, { Component } from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookList from './Components/BookList'
+import * as BooksAPI from './BooksAPI'
 
 class BooksApp extends React.Component {
   state = {
-    bookList: [
-      {
-        id: 'enders',
-        title: 'Enders Game',
-        author: 'Orson Scott Card',
-        backgroundPictureURL: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-        shelf: 'currentlyReading'
-      },
-      {
-        id: 'bird',
-        title: 'To Kill a Mocking Bird',
-        author: 'Harper Lee',
-        backgroundPictureURL: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
-        shelf: 'currentlyReading'
-      },
-      {
-        id: '1776',
-        title: '1776',
-        author: 'David McCullough',
-        backgroundPictureURL: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
-        shelf: 'wantToRead'
-      }, 
-      {
-        id: 'potter',
-        title: 'Harry Potter and the Sorcerers Stone',
-        author: 'JK Rowling',
-        backgroundPictureURL: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
-        shelf: 'wantToRead'
-      },
-      {
-        id: 'go',
-        title: 'Oh, the Places Youll Go!',
-        author: 'Suess',
-        backgroundPictureURL: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
-        shelf: 'read'
-      },
-      {
-        id: 'hobbit',
-        title: 'The Hobbit',
-        author: 'JRR Tolkien',
-        backgroundPictureURL: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
-        shelf: 'read'
-      },
-    ],
+    bookList: [],
 
 
 
@@ -61,10 +19,24 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
 
-  // moveBook (book, destinationList){
-  //   console.log(book);
-  //   console.log(destinationList);
-  // }
+  componentDidMount(){
+    BooksAPI.getAll()
+      .then((bookList) => {
+        this.setState (() => ({
+          bookList
+        }))
+      })
+  }
+
+  moveBook = (book, destinationShelf) => {
+        BooksAPI.update(book, destinationShelf)
+          .then((bookList) => {
+            this.setState (() => ({
+              bookList
+            }))
+          })
+    }
+
 
 
   render() {
--- MyReads/src/Components/BookList.js
+++ MyReads/src/Components/BookList.js
@@ -1,54 +1,29 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
+import Book from './Book'
 
 class BookList extends React.Component {
     static propTypes = {
-        listTitle: PropTypes.object.isRequired,
+        listTitle: PropTypes.string.isRequired,
         onMoveBook: PropTypes.func.isRequired,
-        books: PropTypes.object.isRequired,
-        filterKey: PropTypes.object.isRequired
+        books: PropTypes.array.isRequired,
+        filterKey: PropTypes.string.isRequired
     }    
-
-
-    handleChange(book){
-
-        console.log(book);
-    };
 
     filterList(books, filterKey){
         return books.filter(book => book.shelf.toUpperCase() === filterKey.toUpperCase())
     };
 
     render() {
-
         const bookList = this.filterList(this.props.books, this.props.filterKey);
 
-        return <div className="bookshelf">
+        return  <div className="bookshelf">
             <h2 className="bookshelf-title">{this.props.listTitle}</h2>
             <div className="bookshelf-books">
                 <ol className="books-grid">
-                    {bookList.map((book) => (
-                        <li key={book.id}>
-                             <div className="book">
-                                 <div className="book-top">
-                                     <div   className="book-cover" 
-                                            style={{ width: 128, 
-                                                     height: 188, 
-                                                     backgroundImage: `url(${book.backgroundPictureURL})`}}></div>
-                                     <div className="book-shelf-changer">
-                                         <select onChange={this.handleChange}>
-                                             <option value="none" disabled>Move to...</option>
-                                             <option value="currentlyReading">Currently Reading</option>
-                                             <option value="wantToRead">Want to Read</option>
-                                             <option value="read">Read</option>
-                                             <option value="none">None</option>
-                                         </select>
-                                     </div>
-                                 </div>
-                                <div className="book-title">{book.title}</div>
-                                <div className="book-authors">{book.author}</div>
-                            </div>
-                        </li>
+                    {bookList.map((bookInstance) => (
+                        <Book book={bookInstance}
+                              onMoveBook={this.props.onMoveBook}/>
                         ))
                     }
                 </ol>

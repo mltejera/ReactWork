fe6f1513cfac53d55f15934eb0730389ebe3a737
Mason Tejera <mltejera@gmail.com>
2018-05-16 16:12:39 -0700

    liking tweets

--- Chirper/src/actions/tweets.js
+++ Chirper/src/actions/tweets.js
@@ -1,8 +1,35 @@
+import { saveLikeToggle } from '../utils/api'
+
+
+export const TOGGLE_TWEET = 'TOGGLE_TWEET'
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
 
-export function receiveTweets (tweets) {
+export function receiveTweets(tweets) {
     return {
         type: RECEIVE_TWEETS,
         tweets,
+    }
+}
+
+function toggleTweet({ id, authedUser, hasLiked }) {
+    return {
+        type: TOGGLE_TWEET,
+        id,
+        authedUser,
+        hasLiked
+    }
+}
+
+export function handleToggleTweet(info) {
+    return (dispatch) => {
+        dispatch(toggleTweet(info))
+
+        return saveLikeToggle(info)
+            .catch((e) => {
+                console.warn('Errpr in handleToggleTweet: ', e)
+                dispatch(toggleTweet(info))
+                alert('there was an error liking the tweet. try again')
+
+            })
     }
 }
--- Chirper/src/components/App.js
+++ Chirper/src/components/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div>
-      <LoadingBar />
+        <LoadingBar />
 
         {this.props.loading === true
           ? null

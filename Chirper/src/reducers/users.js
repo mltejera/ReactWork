88e366dab36144ddacda313ea6d23709a697cd4d
Mason Tejera <mltejera@gmail.com>
2018-05-16 12:34:02 -0700

    rendering tweet ids

--- Chirper/src/reducers/users.js
+++ Chirper/src/reducers/users.js
@@ -1,8 +1,8 @@
-import { RECIEVE_USERS } from '../actions/users'
+import { RECEIVE_USERS } from '../actions/users'
 
 export default function users (state = {}, action) {
     switch(action.type){
-        case RECIEVE_USERS:
+        case RECEIVE_USERS:
             return {
                 ...state,
                 ...action.users

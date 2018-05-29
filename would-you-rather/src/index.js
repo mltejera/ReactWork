f988502f66e3db9a38fc316838f0a632c6730e34
Mason Tejera <mltejera@gmail.com>
2018-05-28 19:21:24 -0700

    we have data!
    set up actions, reducers, installed redux, thunk, middleware, etc

--- would-you-rather/src/index.js
+++ would-you-rather/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import middleware from './middleware'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, middleware)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
